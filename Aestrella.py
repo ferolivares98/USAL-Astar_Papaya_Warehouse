1584bcbbc70b3c46eb1749fd1564b69b75d69523
Fernando Olivares <ferolinaranjo@gmail.com>
2021-12-09 20:12:28 +0100

    Entrega A Estrella

--- Aestrella.py
+++ Aestrella.py
@@ -1,11 +1,11 @@
+# Algoritmo A estrella - Papaya Warehouse
 # Fernando Olivares Naranjo
-
-DEBUG = False
-
-def heu(nodo_in, nodo_fi):
-    return -(nodo_fi.precio - nodo_in.precio)
+# Diego Sánchez Martín
 
 
+# ---------------------------------------------------------------------------------------------------------
+# Clase Bandeja (los nodos)
+# ---------------------------------------------------------------------------------------------------------
 class NodoBandeja:
     def __init__(self, peso, precio, lista_papayas):
         self.peso = peso
@@ -19,6 +19,9 @@ class NodoBandeja:
         return self.g + self.h
 
 
+# ---------------------------------------------------------------------------------------------------------
+# Minimiza f (el precio) y maximiza el peso respecto al límite necesario (2kg)
+# ---------------------------------------------------------------------------------------------------------
 def nodo_minimiza_f(lista_abiertos):
     nodo_minimo = lista_abiertos[0]
     for nodo in lista_abiertos:
@@ -27,68 +30,73 @@ def nodo_minimiza_f(lista_abiertos):
     return nodo_minimo
 
 
-def getNodo(identificador_bandeja_sig, nodos):
+# ---------------------------------------------------------------------------------------------------------
+# Comprobación para obtención de nodo_sig.
+# ---------------------------------------------------------------------------------------------------------
+def get_nodo(identificador_bandeja_sig, nodos):
     for nodo in nodos:
         if identificador_bandeja_sig in nodo.lista_papayas:
             return nodo
     return None
 
 
-def calculoHeu(nodo_actual, nodo_sig):
+# ---------------------------------------------------------------------------------------------------------
+# Heurística: minimiza precio y se maximiza peso respecto a los 2kg.
+# ---------------------------------------------------------------------------------------------------------
+def heu(nodo_in, nodo_fi):
+    return nodo_fi.precio - nodo_in.precio
+
+
+def calculo_heuristica(nodo_actual, nodo_sig):
     return heu(nodo_actual, nodo_sig)
 
 
+# ---------------------------------------------------------------------------------------------------------
+# Algoritmo A estrella. Implementación similar a la vista en clase, con salida de éxito ante
+# cumplimiento de condiciones de bandeja y peso.
+# ---------------------------------------------------------------------------------------------------------
 def a_estrella(inicio, fin, nodos):
-    open_list = []
-    close_list = []
+    lista_abierta = []
+    lista_cerrada = []
     nodo_actual = inicio
     nodo_actual.h = heu(inicio, fin)
-    open_list.append(inicio)
-    while len(open_list) > 0:
-        nodo_actual = nodo_minimiza_f(open_list)
-        if nodo_actual.peso > 2:
+    lista_abierta.append(inicio)
+    while len(lista_abierta) > 0:
+        nodo_actual = nodo_minimiza_f(lista_abierta)
+        if nodo_actual.peso > fin.peso:
             break
         for bandeja_sig_ID in nodos:
-            #nodo_sig = getNodo(bandeja_sig_ID, nodos)
+            # nodo_sig = getNodo(bandeja_sig_ID, nodos)
             nodo_sig = bandeja_sig_ID
-            papaya_sig_coste_actual = nodo_actual.g + calculoHeu(nodo_actual, nodo_sig)
-            #papaya_sig_coste_actual = nodo_actual.g + calculoHeu(nodo_actual, nodo_sig)
-            if nodo_sig in open_list:
+            papaya_sig_coste_actual = nodo_actual.g + calculo_heuristica(nodo_actual, nodo_sig)
+            if nodo_sig in lista_abierta:
                 if nodo_sig.g <= papaya_sig_coste_actual:
                     continue
-            elif nodo_sig in close_list:
+            elif nodo_sig in lista_cerrada:
                 if nodo_sig.g <= papaya_sig_coste_actual:
                     continue
-                close_list.remove(nodo_sig)
-                open_list.append(nodo_sig)
+                lista_cerrada.remove(nodo_sig)
+                lista_abierta.append(nodo_sig)
             else:
-                open_list.append(nodo_sig)
+                lista_abierta.append(nodo_sig)
                 nodo_sig.h = heu(nodo_sig, fin)
             nodo_sig.g = papaya_sig_coste_actual
             nodo_sig.padre = nodo_actual
-        open_list.remove(nodo_actual)
-        close_list.append(nodo_actual)
+
+        lista_abierta.remove(nodo_actual)
+        lista_cerrada.append(nodo_actual)
 
     if nodo_actual.peso < fin.peso:
         return None
     else:
         return nodo_actual
-        # path = []
-        # while nodo_actual is not None:
-        #    path.append(nodo_actual)
-        #    nodo_actual = nodo_actual.padre
-        # return path
 
 
+# ---------------------------------------------------------------------------------------------------------
+# Clase Papaya.
+# ---------------------------------------------------------------------------------------------------------
 class Papaya:
     def __init__(self, identificador, peso, dias):
         self.identificador = identificador
         self.peso = peso
         self.dias = dias
-
-
-def debug(msg):
-    if DEBUG:
-        print(msg)
-    else:
-        pass
